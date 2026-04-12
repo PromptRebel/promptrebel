363894806c3aac60928e57d4e10737c61d2fb0c6
PromptRebel <excelcrackhead@gmail.com>
2026-04-12 11:07:41 +0200

    Update renderer.js

--- tools/games/rts/renderer.js
+++ tools/games/rts/renderer.js
@@ -5,7 +5,6 @@ const Renderer = {
     draw: function() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        // Gebäude
         GameState.entities.buildings.forEach(b => {
             this.ctx.fillStyle = b.isFinished ? (b.type === 'house' ? '#8B4513' : '#2F4F4F') : '#555';
             this.ctx.fillRect(b.x - 20, b.y - 20, 40, 40);
@@ -18,13 +17,22 @@ const Renderer = {
             this.ctx.fillText(b.type === 'house' ? 'Haus' : 'Forst', b.x - 15, b.y + 5);
         });
 
-        // Bäume
+        if (GameState.placementMode.active) {
+            this.ctx.globalAlpha = 0.5;
+            this.ctx.fillStyle = 'white';
+            this.ctx.fillRect(GameState.placementMode.x - 20, GameState.placementMode.y - 20, 40, 40);
+            if (GameState.placementMode.type === 'lodge') {
+                this.ctx.strokeStyle = 'white';
+                this.ctx.beginPath(); this.ctx.arc(GameState.placementMode.x, GameState.placementMode.y, 90, 0, Math.PI*2); this.ctx.stroke();
+            }
+            this.ctx.globalAlpha = 1.0;
+        }
+
         GameState.entities.trees.forEach(t => {
             this.ctx.fillStyle = '#2d5a27';
             this.ctx.beginPath(); this.ctx.arc(t.x, t.y, 10, 0, Math.PI * 2); this.ctx.fill();
         });
 
-        // HQ & Villager
         const tc = GameState.entities.townCenter;
         this.ctx.fillStyle = '#654321'; this.ctx.fillRect(tc.x - 25, tc.y - 25, 50, 50);
         GameState.entities.villagers.forEach(v => {
