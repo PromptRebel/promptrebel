b531ed8b3cda66f0a6d0a446f216adbd03064bb8
PromptRebel <excelcrackhead@gmail.com>
2026-04-12 09:35:26 +0200

    Update main.js

--- tools/games/rts/main.js
+++ tools/games/rts/main.js
@@ -1,6 +1,5 @@
 function init() {
-    // Erstelle Bäume mit Holz-Limit
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 15; i++) {
         GameState.entities.trees.push({
             x: Math.random() * 700 + 50,
             y: Math.random() * 500 + 50,
@@ -9,11 +8,9 @@ function init() {
     }
     gameLoop();
 }
-
 function gameLoop() {
     GameState.entities.villagers.forEach(v => v.update());
     Renderer.draw();
     requestAnimationFrame(gameLoop);
 }
-
 init();
