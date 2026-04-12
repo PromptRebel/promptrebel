7f60fdbcbea5304d4b97b318d0f060f7b7cfb41b
PromptRebel <excelcrackhead@gmail.com>
2026-04-12 11:06:14 +0200

    Update gamestate.js

--- tools/games/rts/gamestate.js
+++ tools/games/rts/gamestate.js
@@ -7,14 +7,18 @@ const GameState = {
         buildings: [] 
     },
     selection: null,
+    placementMode: {
+        active: false,
+        type: null,
+        x: 0,
+        y: 0,
+        cost: 0
+    },
     config: {
         baseMaxPop: 10,
         villagerSpeed: 2,
         treeWoodAmount: 50,
-        costs: { 
-            house: 50, 
-            lodge: 100 
-        }
+        costs: { house: 50, lodge: 100 }
     },
     getMaxPop: function() {
         let bonus = 0;
