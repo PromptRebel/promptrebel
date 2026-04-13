7bcedb920880853103df4c4606d7ec911f7658d4
PromptRebel <excelcrackhead@gmail.com>
2026-04-13 10:27:13 +0200

    Update renderer.js

--- tools/games/rts/renderer.js
+++ tools/games/rts/renderer.js
@@ -1,36 +1,9 @@
-import { GameState } from './gamestate.js'; // Der Renderer braucht den State!
-
+import { GameState } from './gamestate.js';
 
 export const Renderer = {
     canvas: document.getElementById('gameCanvas'),
     ctx: document.getElementById('gameCanvas').getContext('2d'),
     assets: {},
-
-    drawSketchLine: function(x1, y1, x2, y2, color = '#331a00', width = 2) {
-        this.ctx.strokeStyle = color;
-        this.ctx.lineWidth = width;
-        this.ctx.beginPath();
-        this.ctx.moveTo(x1, y1);
-        const midX = (x1 + x2) / 2 + (Math.random() * 4 - 2);
-        const midY = (y1 + y2) / 2 + (Math.random() * 4 - 2);
-        this.ctx.lineTo(midX, midY);
-        this.ctx.lineTo(x2, y2);
-        this.ctx.stroke();
-    },
-
-    drawSketchPath: function(points, fillColor) {
-        this.ctx.fillStyle = fillColor;
-        this.ctx.beginPath();
-        this.ctx.moveTo(points[0].x, points[0].y);
-        points.forEach(p => this.ctx.lineTo(p.x, p.y));
-        this.ctx.closePath();
-        this.ctx.fill();
-        for (let i = 0; i < points.length; i++) {
-            const start = points[i];
-            const end = points[(i + 1) % points.length];
-            this.drawSketchLine(start.x, start.y, end.x, end.y);
-        }
-    },
 
     draw: function() {
         this.canvas.width = window.innerWidth;
@@ -40,44 +13,48 @@ export const Renderer = {
         this.ctx.save();
         this.ctx.translate(-GameState.camera.x, -GameState.camera.y);
 
-        // Steine zeichnen
+        // 1. STEINE
         GameState.entities.stones.forEach(s => {
-            const img = this.assets.props ? this.assets.props.stone : null;
-            if (img) {
-                this.ctx.drawImage(img, s.x - 25, s.y - 25, 50, 50);
-            } else {
-                this.ctx.fillStyle = 'gray';
-                this.ctx.beginPath(); this.ctx.arc(s.x, s.y, 15, 0, Math.PI*2); this.ctx.fill();
-            }
+            const img = this.assets.props?.stone;
+            if (img) this.ctx.drawImage(img, s.x - 25, s.y - 25, 50, 50);
         });
 
-        // Bäume zeichnen
+        // 2. BÄUME
         GameState.entities.trees.forEach(t => {
-            const img = this.assets.props ? this.assets.props.tree : null;
+            const img = this.assets.props?.tree;
             if (img) this.ctx.drawImage(img, t.x - 30, t.y - 50, 60, 60);
         });
 
-        // Gebäude (HQ)
-        const tc = GameState.entities.townCenter;
-        this.ctx.save();
-        this.ctx.translate(tc.x, tc.y);
-        this.drawSketchPath([{x:-35,y:-20},{x:35,y:-20},{x:40,y:25},{x:-40,y:25}], '#654321');
-        this.drawSketchPath([{x:-40,y:-20},{x:0,y:-55},{x:40,y:-20}], '#A0522D');
-        this.ctx.restore();
-
-        // Villager
-        GameState.entities.villagers.forEach(v => {
-            if (GameState.selection === v) {
-                this.ctx.strokeStyle = 'white';
-                this.ctx.strokeRect(v.x - 12, v.y - 12, 24, 24);
+        // 3. GEBÄUDE (Haus & Forsthaus)
+        GameState.entities.buildings.forEach(b => {
+            const img = b.type === 'house' ? this.assets.props?.house : this.assets.props?.lodge;
+            if (img) {
+                this.ctx.globalAlpha = b.isFinished ? 1.0 : 0.5;
+                this.ctx.drawImage(img, b.x - 40, b.y - 40, 80, 80);
+                this.ctx.globalAlpha = 1.0;
             }
-            this.ctx.fillStyle = '#ffdf00';
+            if (!b.isFinished) {
+                this.ctx.fillStyle = "green";
+                this.ctx.fillRect(b.x - 20, b.y - 50, b.progress * 0.4, 5);
+            }
+        });
+
+        // 4. HQ
+        const tc = GameState.entities.townCenter;
+        const hqImg = this.assets.props?.hq;
+        if (hqImg) {
+            this.ctx.drawImage(hqImg, tc.x - 60, tc.y - 60, 120, 120);
+        }
+
+        // 5. VILLAGER
+        GameState.entities.villagers.forEach(v => {
+            this.ctx.fillStyle = GameState.selection === v ? 'white' : 'yellow';
             this.ctx.fillRect(v.x - 6, v.y - 6, 12, 12);
         });
 
         this.ctx.restore();
-        
-        // UI Update
+
+        // UI
         document.getElementById('wood-count').innerText = GameState.resources.wood;
         document.getElementById('stone-count').innerText = GameState.resources.stone;
         document.getElementById('pop-count').innerText = GameState.entities.villagers.length + "/" + GameState.getMaxPop();
